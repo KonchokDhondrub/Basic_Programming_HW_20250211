fe6eb451877b3957f9aa6cc978420d7747e3dd25
kd <erm.melnik@gmail.com>
2025-02-11 19:16:07 +0100

    Initial commit. Comments added.

---
+++ src/Email.java
@@ -0,0 +1,3 @@
+public class Email implements MailDeliveryService {
+ // Его способ отправки почты - напечатать что-то вроде: «Отправить по Интернету».
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Реализуйте диалог с пользователем (использовать Scanner),
+        // как отправлять почту и отправлять ее с помощью Sender.
+    }
+}
