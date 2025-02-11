3674aee78e8e7442c536708f48ddd1c3203592c2
kd <erm.melnik@gmail.com>
2025-02-11 20:27:34 +0100

    Added methods menu(), send(), getOperationName().

--- src/Sender.java
+++ src/Sender.java
@@ -1,4 +1,24 @@
-public class Sender {
-    // Реализуйте в нем метод send(), который в качестве аргумента принимает MailDeliveryService
-    // и отправляет почту с помощью данной службы.
+import java.util.Scanner;
+
+public class Sender implements MailDeliveryService{
+
+    public String getOperationName(){
+        return "";
+    }
+    public String send(){
+        return "";
+    }
+
+    public MailDeliveryService menu(MailDeliveryService[] senders, Scanner scanner){
+        System.out.println("Варианты доставки:" );
+        for (int i = 0; i< senders.length;i++){
+            System.out.printf("%d. %s \n", i+1, senders[i].getOperationName());
+        }
+        System.out.print("Выберите способ доставки: ");
+        int select = 0;
+        do {
+            select = scanner.nextInt() - 1;
+        } while (select < 0 || select >= senders.length);
+        return senders[select];
+    }
 }
