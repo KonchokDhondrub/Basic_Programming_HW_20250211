423ee361bfdd482c1a7a3ea1b72a40e4de6fe540
kd <erm.melnik@gmail.com>
2025-02-11 20:39:52 +0100

    initial commit.

---
+++ HW2/src/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    String fName;
+    String lName;
+    int age;
+    String email;
+
+    public Person(String fName, String lName, int age, String email) {
+        this.fName = fName;
+        this.lName = lName;
+        this.age = age;
+        this.email = email;
+    }
+}
