28b5c4e9c7862cc3dfbab83bea3a7362c1b0708a
kd <erm.melnik@gmail.com>
2025-02-11 20:26:32 +0100

    Added Classes, menu(), print result.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Реализуйте диалог с пользователем (использовать Scanner),
-        // как отправлять почту и отправлять ее с помощью Sender.
+        Scanner scanner = new Scanner(System.in);
+        MailDeliveryService[] senders = {
+                new DHL(),
+                new Email(),
+                new Pigeon()
+        };
+        Sender sendOperator = new Sender();
+
+        Sender result = (Sender) sendOperator.menu(senders, scanner);
+        System.out.printf(result.send());
     }
 }
